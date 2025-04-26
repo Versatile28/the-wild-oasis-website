4dfd00d757daa504b8bfb026224638d45b026cdf
Satyaki Pal <satyakipal111@gmail.com>
2025-04-26 23:06:57 +0530

    some of thr frontend is done

--- app/account/page.js
+++ app/account/page.js
@@ -4,7 +4,9 @@ export const metadata = {
 
 export default function Page() {
    return (
-     <h1>Accounts page</h1>
+      <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+      Your reservations
+    </h2>
    )
  }
  
