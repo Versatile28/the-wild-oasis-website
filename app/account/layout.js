912f7880e1ba42e713f1ee64199f09c7f62c71ad
Satyaki Pal <satyakipal111@gmail.com>
2025-04-27 21:29:32 +0530

    some more progress

--- app/account/layout.js
+++ app/account/layout.js
@@ -1,10 +1,10 @@
-import SideNavigation from "../_components/SideNavigation";
+import SideNavigation from "@/app/_components/SideNavigation";
 
-export default function layout({ childern }) {
+export default function layout({ children }) {
   return (
     <div className="grid grid-cols-[16rem_1fr] h-full gap-12">
       <SideNavigation />
-      <div className="py-16">{childern}</div>
+      <div className="py-1">{children}</div>
     </div>
   )
 }
